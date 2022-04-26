3226a8fe0b21275a84fd450f0f6bd7a6a3d2ec73
Juha Halmu <juha.halmu@iki.fi>
2022-04-27 00:25:02 +0300

    Put bots in folder, tweeked RP's code for sleeping

---
+++ bots/config.py
@@ -0,0 +1,28 @@
+# twitterbot/bots/config.py
+# thanks to realpython.com
+
+import tweepy
+import logging
+import seacret
+
+logger = logging.getLogger()
+
+def create_api():
+    KEY = seacret.KEY
+    SECRET= seacret.SECRET
+    TOKEN = seacret.TOKEN
+    TOKEN_SECRET = seacret.TOKEN_SECRET
+
+    # Authenticate to Twitter
+    auth = tweepy.OAuthHandler(KEY, SECRET)
+    auth.set_access_token(TOKEN, TOKEN_SECRET)
+
+    api = tweepy.API(auth)
+
+    try:
+        api.verify_credentials()
+    except Exception as e:
+        logger.error("Error creating API", exc_info=True)
+        raise e
+    logger.info("API created")
+    return api
---
+++ bots/followfollowers.py
@@ -0,0 +1,34 @@
+# twitterbot/bots/followfollowers.py 
+
+import tweepy
+import logging
+from config import create_api
+import pause
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+def follow_followers(api):
+    logger.info("Retrieving and following followers")
+    for follower in tweepy.Cursor(api.get_followers).items():
+
+        # if no followers ---
+        if follower.following == False:
+            if not follower.following:
+                logger.info(f"Following {follower.name}")
+                follower.follow()
+        else:
+
+            # -- lets check tomorrow again
+            logger.info("No followers, going to sleep for 24h")
+            pause.days(1)
+
+def main():
+    api = create_api()
+    while True:
+        follow_followers(api)
+        logger.info("Waiting...")
+        pause.minutes(1)
+
+if __name__ == "__main__":
+    main()
