0ab0f072c4e9c5f5eb76f57299f3194924a32181
Juha Halmu <juha.halmu@iki.fi>
2022-04-26 14:07:22 +0300

    Adding .gitignore

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import tweepy
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
+auth.set_access_token("ACCESS_TOKEN", "ACCESS_SECRET")
+api = tweepy.API(auth)
+
+try:
+    api.verify_credentials()
+    print("Authentication Successful")
+except:
+    print("Authentication Error")
